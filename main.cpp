3fdbde33544f40b11159a0bc099aa1a9764ebe86
Karina <volsu.esenova@gmail.com>
2019-06-06 17:40:07 +0400

    Yopta 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+using namespace std;
+//Это учебная программа
 int main(){
+	int a, b;
+	cout << "Enter a:";
+	cin >> a;
+	cout << "\nEnter b:";
+	cin >> b;
+	cout << "\na/b=" << a/b;
 	return 0;
 }
